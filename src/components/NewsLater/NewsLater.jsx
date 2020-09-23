12bb7b97f189e65d97e954db711f3807b0cda7fa
asit singh <asitsingh0@gmail.com>
2020-09-23 19:30:04 +0530

    myapp

---
+++ src/components/NewsLater/NewsLater.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./NewsLater.css";
+const NewsLater = () => {
+  return (
+    <>
+      <div className="NewsLater_bg">
+        <div className="">
+          <h1>Join Thousand Of Happy Students!</h1>
+          <p>Subscribe our newsletter & get latest news and updation!</p>
+          <input
+            type="email"
+            className="NewsLater_bg_input"
+            placeholder="Your Email Address"
+          />
+          <input type="submit" className="NewsLater_bg_input2" />
+        </div>
+      </div>
+    </>
+  );
+};
+export default NewsLater;
